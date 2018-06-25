32a629e2b9c78d878334238b336c289ee7567203
Rafael <exposistemasdesarrollo@gmail.com>
2018-06-25 13:05:20 -0400

    cambios vario

--- app/Modelos/Proyecto.php
+++ app/Modelos/Proyecto.php
@@ -25,13 +25,13 @@ class Proyecto extends Model
 
       public static function Consulta_proyecto($id){
         $features = DB::table('solicitudes')
-            ->select('proyectos.id as id','proyecto_type.nombre AS tipo','solicitudes.nombre AS nombre','proyectos.created_at AS created_at','responsables.nombre AS responsable')
+            ->select('proyectos.id as id','proyecto_type.nombre AS tipo','solicitudes.descripcion AS descripcion','solicitudes.nombre AS nombre','proyectos.created_at AS created_at','responsables.nombre AS responsable')
             ->leftJoin('proyectos', 'solicitudes.id', '=', 'proyectos.solicitud_id')
             ->leftJoin('responsable_proyecto', 'proyectos.id', '=', 'responsable_proyecto.proyecto_id')
             ->leftJoin('responsables', 'responsable_proyecto.responsable_id', '=', 'responsables.id')
             ->leftJoin('proyecto_type', 'solicitudes.proyecto_type_id', '=', 'proyecto_type.id')
             ->where('solicitudes.id','=',$id)  
-            ->get();    
+            ->first();    
         return $features;
       }
 }
